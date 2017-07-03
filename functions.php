c51b96abab8e9898de6db3bdd9274fa6e14c5553
saazaan7 <sajanjoshi0@gmail.com>
2017-07-03 17:08:20 +0545

    Registered and enqueued styles and scripts

--- functions.php
+++ functions.php
@@ -117,6 +117,8 @@ add_action( 'widgets_init', 'whank_widgets_init' );
  * Enqueue scripts and styles.
  */
 function whank_scripts() {
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
 	wp_enqueue_style( 'whank-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'whank-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
@@ -126,8 +128,47 @@ function whank_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	/*Enqueue additional css - fontawesome, bootstrap, swiper*/
+	wp_enqueue_style( 'fontawesome', WHANK_CSS_URL.'/font-awesome'.$suffix.'.css', array(), false, all );
+	wp_enqueue_style( 'bootstrap-style', WHANK_CSS_URL.'/bootstrap'.$suffix.'.css', array(), 3.3, all );
+	wp_register_style( 'swiper-style', WHANK_CSS_URL.'/swiper'.$suffix.'.css', array(), 3.4, all );
+	wp_enqueue_style( 'custom-style', WHANK_CSS_URL.'/custom.css', array(),'', all );
+	/*Enqueue scripts*/
+	wp_register_script( 'swiper-script', WHANK_JS_URL. '/swiper.jquery'.$suffix.'.js', array('jquery'), '3.4', true );
+	wp_enqueue_script( 'bootstrap-script', WHANK_JS_URL. '/bootstrap'.$suffix.'.js', array('jquery'), '3.3', true );
 }
 add_action( 'wp_enqueue_scripts', 'whank_scripts' );
+
+/**
+ * Define Directory Location Constants
+ */
+define( 'WHANK_PARENT_DIR', get_template_directory() );
+define( 'WHANK_INCLUDES_DIR', WHANK_PARENT_DIR. '/inc' );
+define( 'WHANK_CSS_DIR', WHANK_PARENT_DIR . '/css' );
+define( 'WHANK', WHANK_PARENT_DIR . '/js' );
+define( 'WHANK_LANGUAGES_DIR', WHANK_PARENT_DIR . '/languages' );
+
+define( 'WHANK_ADMIN_DIR', WHANK_INCLUDES_DIR . '/admin' );
+define( 'WHANK_WIDGETS_DIR', WHANK_INCLUDES_DIR . '/widgets' );
+
+define( 'WHANK_ADMIN_IMAGES_DIR', WHANK_ADMIN_DIR . '/images' );
+/**
+ * Define URL Location Constants
+ */
+define( 'WHANK_PARENT_URL', get_template_directory_uri() );
+define( 'WHANK_CHILD_URL', get_stylesheet_directory_uri() );
+
+define( 'WHANK_INCLUDES_URL', WHANK_PARENT_URL. '/inc' );
+define( 'WHANK_CSS_URL', WHANK_PARENT_URL . '/css' );
+define( 'WHANK_JS_URL', WHANK_PARENT_URL . '/js' );
+define( 'WHANK_LANGUAGES_URL', WHANK_PARENT_URL . '/languages' );
+
+define( 'WHANK_ADMIN_URL', WHANK_INCLUDES_URL . '/admin' );
+define( 'WHANK_WIDGETS_URL', WHANK_INCLUDES_URL . '/widgets' );
+
+define( 'WHANK_ADMIN_IMAGES_URL', WHANK_ADMIN_URL . '/images' );
+
 
 /**
  * Implement the Custom Header feature.
