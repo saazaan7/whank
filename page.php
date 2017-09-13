6798768c9b2e388f13f16ff12a8f646b69bc2d12
saazaan7 <sajanjoshi0@gmail.com>
2017-09-13 22:11:54 +0545

    Managed page template layout

--- page.php
+++ page.php
@@ -13,26 +13,30 @@
  */
 
 get_header(); ?>
+	<?php do_action( 'whank_before_body_content' ); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area col-md-8">
 		<main id="main" class="site-main">
+			<div class="container-fluid">
+				<div class="row">
+				<?php
+					while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/content', 'page' );
 
-				get_template_part( 'template-parts/content', 'page' );
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
+					endwhile; // End of the loop.
+				?>
+				</div>
+				</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
 get_sidebar();
+do_action( 'whank_before_body_content' );
 get_footer();
